d57b3eab0057b35ded14f711f85b6c25198f5f0d
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-05-21 05:40:27 +0100

    :[Only HvsH]

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,108 +1,48 @@
 import React from "react";
 import CheckWinner from "./CheckWinner";
-import { useState, useEffect } from "react";
-import ComputerPattern from "./ComputerPattern";
+import { useState } from "react";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  // const [isXNext, setIsXNext] = useState(false);
-  const randomNumbers  = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8,
-  ];
-  let random = 4;
-  let timeId;
-  const [click, setClick] = useState(true);
-  let nextSquare = squares.slice();
-  const [Oset, setOset] = useState(false);
+  const [isXNext, setIsXNext] = useState(false);
 
-  useEffect(() => {
-    return () => {
-      clearTimeout(timeId);
-    };
-  }, [Oset, timeId]);
-  //   function handleClick(i) {
-  //     const nextSquare = squares.slice();
-  //     const randomNumber = generateRandomNumber(0, 9);
-  //     if (CheckWinner(nextSquare)) {
-  //       return;
-  //     }
-
-  //     if (nextSquare[i] === null) {
-  //       if (!isXNext) {
-  //         nextSquare[i] = "X";
-  //       } else if (isXNext) {
-  //         nextSquare[i] = "O";
-  //       }
-  //       console.log(randomNumber);
-  //     }
-
-  //     setSquares(nextSquare);
-  //     setIsXNext(!isXNext);
-  //     CheckWinner(nextSquare);
-
-  //     if (CheckWinner(nextSquare) === "X") {
-  //       console.log("X is the Winner!");
-  //     } else if (CheckWinner(nextSquare) === "O") {
-  //       console.log("O is the Winner!");
-  //     } else {
-  //       return;
-  //     }
-  //   }
-
-  function getRandom(min, max) {
-    const floatRandom = Math.random();
-
-    const difference = max - min;
-
-    // random between 0 and the difference
-    const random = Math.floor(difference * floatRandom) + min;
-
-    return random;
+  if (squares.every((v) => v !== null) && !CheckWinner(squares)) {
+    document.querySelector("#notify").innerText = "It is a Draw!";
   }
 
-  function noRepetition() {
-    let randomIndex = getRandom(0, randomNumbers.length - 1);
-    let cut = randomNumbers[randomIndex];
-    randomNumbers.splice(randomIndex, 1);
-    random = cut;
+  function Reset(){
+    setSquares(Array(9).fill(null))
+    setIsXNext(false)
+    document.querySelector("#notify").innerText = "Player X to play";
+    return;
   }
 
   function handleClick(i) {
-    
-    if (CheckWinner(nextSquare) || nextSquare[i]) {
+    const nextSquare = squares.slice();
+    document.querySelector('#btns button').addEventListener('click', Reset)
+
+    if (CheckWinner(nextSquare)) {
       return;
     }
+
     if (nextSquare[i] === null) {
-      nextSquare[i] = "X";
-    }
-    if (!ComputerPattern(nextSquare, CheckWinner, i)) {
-      setClick(c => c = false)
-      timeId = setTimeout(() => {
-        if (nextSquare[random] === null) {
-          nextSquare[random] = "O";
-          setOset(!Oset);
-          setClick(c => c = true)
-        } else if (nextSquare[random] !== null) {
-          for (let i = 0; i < randomNumbers.length; i++) {
-            noRepetition();
-            if (nextSquare[random] === null && !CheckWinner(nextSquare)) {
-              nextSquare[random] = "O";
-              setOset(!Oset);
-              setClick(c => c = true)
-              break;
-            }
-          }
-        }
-      }, 2000);
+      if (!isXNext) {
+        nextSquare[i] = "X";
+        document.querySelector("#notify").innerText = "Player O to play";
+      } else if (isXNext) {
+        nextSquare[i] = "O";
+        document.querySelector("#notify").innerText = "Player X to play";
+      }
     }
 
     setSquares(nextSquare);
+    setIsXNext(!isXNext);
     CheckWinner(nextSquare);
 
     if (CheckWinner(nextSquare) === "X") {
-      console.log("X is the Winner!");
+      document.querySelector("#notify").innerText = "Player X is the Winner!";
     } else if (CheckWinner(nextSquare) === "O") {
-      console.log("O is the Winner!");
+      document.querySelector("#notify").innerText = "Player O is the Winner!";
     } else {
       return;
     }
@@ -111,103 +51,41 @@ export default function Board() {
   return (
     <section className="Board">
       <section className="group-rows">
-        {click ? (
-          <section className="squares" id="0" onClick={() => handleClick(0)}>
-            {squares[0]}
-          </section>
-        ) : (
-          <section className="squares" id="0">
-            {squares[0]}
-          </section>
-        )}
-
-        {click ? (
-          <section className="squares" id="1" onClick={() => handleClick(1)}>
-            {squares[1]}
-          </section>
-        ) : (
-          <section className="squares" id="1">
-            {squares[1]}
-          </section>
-        )}
-
-        {click ? (
-          <section className="squares" id="2" onClick={() => handleClick(2)}>
-            {squares[2]}
-          </section>
-        ) : (
-          <section className="squares" id="2">
-            {squares[2]}
-          </section>
-        )}
+        <section className="squares" id="0" onClick={() => handleClick(0)}>
+          {squares[0]}
+        </section>
+        <section className="squares" id="1" onClick={() => handleClick(1)}>
+          {squares[1]}
+        </section>
+        <section className="squares" id="2" onClick={() => handleClick(2)}>
+          {squares[2]}
+        </section>
       </section>
 
       <section className="group-rows">
-      {click ? (
-          <section className="squares" id="3" onClick={() => handleClick(3)}>
-            {squares[3]}
-          </section>
-        ) : (
-          <section className="squares" id="3">
-            {squares[3]}
-          </section>
-        )}
-
-
-        {click ? (
-          <section className="squares" id="4" onClick={() => handleClick(4)}>
-            {squares[4]}
-          </section>
-        ) : (
-          <section className="squares" id="4">
-            {squares[4]}
-          </section>
-        )}
-
-
-        {click ? (
-          <section className="squares" id="5" onClick={() => handleClick(5)}>
-            {squares[5]}
-          </section>
-        ) : (
-          <section className="squares" id="5">
-            {squares[5]}
-          </section>
-        )}
+        <section className="squares" id="3" onClick={() => handleClick(3)}>
+          {squares[3]}
+        </section>
+        <section className="squares" id="4" onClick={() => handleClick(4)}>
+          {squares[4]}
+        </section>
+        <section className="squares" id="5" onClick={() => handleClick(5)}>
+          {squares[5]}
+        </section>
       </section>
 
       <section className="group-rows">
-      {click ? (
-          <section className="squares" id="6" onClick={() => handleClick(6)}>
-            {squares[6]}
-          </section>
-        ) : (
-          <section className="squares" id="6">
-            {squares[6]}
-          </section>
-        )}
+        <section className="squares" id="6" onClick={() => handleClick(6)}>
+          {squares[6]}
+        </section>
 
+        <section className="squares" id="7" onClick={() => handleClick(7)}>
+          {squares[7]}
+        </section>
 
-        {click ? (
-          <section className="squares" id="7" onClick={() => handleClick(7)}>
-            {squares[7]}
-          </section>
-        ) : (
-          <section className="squares" id="7">
-            {squares[7]}
-          </section>
-        )}
-
-
-        {click ? (
-          <section className="squares" id="8" onClick={() => handleClick(8)}>
-            {squares[8]}
-          </section>
-        ) : (
-          <section className="squares" id="8">
-            {squares[8]}
-          </section>
-        )}
+        <section className="squares" id="8" onClick={() => handleClick(8)}>
+          {squares[8]}
+        </section>
       </section>
     </section>
   );
