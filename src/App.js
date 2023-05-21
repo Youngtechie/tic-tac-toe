178144d9c8257e82f1e9cc4ba4401f1cd4afc157
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-05-21 05:40:07 +0100

    :[Hot fix]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Game from './components/Game';
 import GameHeader from './components/Game-header';
-import GameSidebar from './components/GameSidebar';
 import './styles/App.css'
 
 export default function TicTacToe() {
+  const player = 'Player X to play'
   return (
     <>
       <main className='container'>
         <GameHeader/>
         <Game/>
-        <GameSidebar/>        
       </main>
+      <p id="notify">{player}</p>
+      <section id='btns'>
+        <button>RESET GAME</button>
+      </section>
     </>
   );
 }
